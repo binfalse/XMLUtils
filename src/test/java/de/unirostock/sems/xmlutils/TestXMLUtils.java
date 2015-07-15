74b4498bf0b670be5e5c6d6960c093a16b0fa310
Martin Scharm <martin@binfalse.de>
2015-07-15 09:30:36 +0100

    created a test suite

---
+++ src/test/java/de/unirostock/sems/xmlutils/TestXMLUtils.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.unirostock.sems.xmlutils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * @author Martin Scharm
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestExamples.class, TestExceptions.class, TestNodeMappers.class, TestNodes.class, TestTreeDocument.class, XmlTest.class })
+public class TestXMLUtils
+{
+	
+}
